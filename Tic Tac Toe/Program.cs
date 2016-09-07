20f95fc3212bc89b28b53b8f93006c1b6cfe7fd5
jthmsquee <christian.ascencio@gmail.com>
2016-09-06 22:44:40 -0700

    tic tac toe hw assignment

---
+++ Tic Tac Toe/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    class Program
+    {
+        private static int[] OneThruNine { get; set; } 
+        static void Main(string[] args)
+        {
+            OneThruNine = new int[8];
+            Console.WriteLine(" | |  ");
+            Console.WriteLine("------");
+            Console.WriteLine(" | |  ");
+            Console.WriteLine("------");
+            Console.WriteLine(" | |  ");
+
+                Console.WriteLine("Player 1 where do you want to put your X?");
+                WhereTheXOrOAre(inputTextHandler, );
+                Console.WriteLine("Player 2 where would you like to play your O?");
+                inputTextHandler();
+        }
+
+        private static int inputTextHandler()
+        {
+            try
+            {
+                int inputText = Convert.ToInt32(Console.ReadLine());
+                if (inputText < 1 || inputText > 9)
+                {
+                    throw new Exception();
+                }
+                return inputText;
+            }
+            catch
+            {
+                Console.WriteLine("Invalid option try again");
+            }
+            return 0;
+        }
+
+        private static void WhereTheXOrOAre(int PlayersInput, int isThisAnXOrAnO)
+        {
+            OneThruNine[PlayersInput - 1] = isThisAnXOrAnO;
+        }
+    }
+}
