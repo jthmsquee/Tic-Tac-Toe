b1194992cb9e55f5bfa5d4039ea45b8f44c4a929
jthmsquee <christian.ascencio@gmail.com>
2016-09-07 22:16:09 -0700

    latest update

--- Tic Tac Toe/Program.cs
+++ Tic Tac Toe/Program.cs
@@ -11,17 +11,24 @@ namespace Tic_Tac_Toe
         private static int[] OneThruNine { get; set; } 
         static void Main(string[] args)
         {
-            OneThruNine = new int[8];
+            OneThruNine = new int[9];
+            int X = 1;
+            int O = 2;
             Console.WriteLine(" | |  ");
             Console.WriteLine("------");
             Console.WriteLine(" | |  ");
             Console.WriteLine("------");
             Console.WriteLine(" | |  ");
+            Console.WriteLine("Player 1 where do you want to put your X?");
+            int input = inputTextHandler();
+            while (input == -1)
+            {
+                input = inputTextHandler();
+            }
+            WhereTheXOrOAre(input, X);
 
-                Console.WriteLine("Player 1 where do you want to put your X?");
-                WhereTheXOrOAre(inputTextHandler, );
-                Console.WriteLine("Player 2 where would you like to play your O?");
-                inputTextHandler();
+
+
         }
 
         private static int inputTextHandler()
@@ -33,18 +40,27 @@ namespace Tic_Tac_Toe
                 {
                     throw new Exception();
                 }
+                if (OneThruNine[inputText - 1] >= 1)
+                {
+                    throw new Exception();
+                }
                 return inputText;
             }
             catch
             {
                 Console.WriteLine("Invalid option try again");
             }
-            return 0;
+            return -1;
         }
 
         private static void WhereTheXOrOAre(int PlayersInput, int isThisAnXOrAnO)
         {
             OneThruNine[PlayersInput - 1] = isThisAnXOrAnO;
+        }
+
+        private static void updatedBoard()
+        {
+
         }
     }
 }
